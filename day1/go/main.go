85126b6e54628c9e3704ca2d945e38f91f559828
J-Hagen-Dev <j.hagen.dev@gmail.com>
2024-02-16 07:38:25 +0200

    Day 1 Go Solution

--- day1/go/main.go
+++ day1/go/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"unicode"
+)
 
+func main() {
+	total := 0
+
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+
+	for {
+		line, _, err := r.ReadLine()
+		if err != nil {
+			break
+		}
+		if len(line) > 0 {
+			runes, s := []rune(string(line[:])), ""
+
+			for _, r := range runes {
+				if unicode.IsDigit(r) {
+					s = string(r)
+					break
+				}
+			}
+
+			for j := len(runes) - 1; j >= 0; j-- {
+				if unicode.IsDigit(runes[j]) {
+					s = s + string(runes[j])
+					break
+				}
+			}
+
+			if c, err := strconv.Atoi(s); err == nil {
+				total += c
+			}
+		}
+	}
+
+	fmt.Printf("Total: %d", total)
 }
