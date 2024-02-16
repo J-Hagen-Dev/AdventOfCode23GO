ba436a4b81716ac0b3d36707fb6f6eb85ab275ed
J-Hagen-Dev <j.hagen.dev@gmail.com>
2024-02-16 12:56:21 +0200

    d1 part 2 solution go

--- day1-part2/go/main.go
+++ day1-part2/go/main.go
@@ -23,22 +23,37 @@ func main() {
 
 	for {
 		line, _, err := r.ReadLine()
+
 		if err != nil {
 			break
 		}
+
 		if len(line) > 0 {
 			runes, s := []rune(string(line[:])), ""
 
-			for _, r := range runes {
+			for i, r := range runes {
+
 				if unicode.IsDigit(r) {
 					s = string(r)
+					break
+				}
+
+				if n := checkAlphabeticalDigits(runes[:i+1]); n > 0 {
+					s = strconv.Itoa(n)
 					break
 				}
 			}
 
 			for j := len(runes) - 1; j >= 0; j-- {
+				//fmt.Println("Backward char", string(runes[j]), "\tindex", j)
+				//fmt.Println("Backward slice", string(runes[j:]), "\tindex", j)
 				if unicode.IsDigit(runes[j]) {
 					s = s + string(runes[j])
+					break
+				}
+
+				if n := checkAlphabeticalDigits(runes[j:]); n > 0 {
+					s = s + strconv.Itoa(n)
 					break
 				}
 			}
@@ -49,5 +64,35 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Total: %d", total)
+	fmt.Printf("Total: %d\n", total)
+}
+
+func checkAlphabeticalDigits(runes []rune) int {
+	rLen := len(runes)
+
+	charNums := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	for i := 3; i <= 5; i++ {
+		if rLen < i {
+			break
+		}
+
+		for key := range charNums {
+			if key == string(runes[rLen-i:]) || key == string(runes[:i]) {
+				return charNums[key]
+			}
+		}
+	}
+
+	return -1
 }
